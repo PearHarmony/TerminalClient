84e805b3fbaf9860807f4c96d5f2d7a35b27feea
Chris <hi@aocalux.dev>
2024-02-23 11:56:38 +0100

    add console read and write

--- src/org/pearharmony/ui/TerminalRead.java
+++ src/org/pearharmony/ui/TerminalRead.java
@@ -1,5 +1,12 @@
 package org.pearharmony.ui;
 
+import java.util.Scanner;
+
 public class TerminalRead {
-    
+
+     Scanner console = new Scanner(System.in);
+
+    public String string(){
+        return console.nextLine();
+    }
 }
