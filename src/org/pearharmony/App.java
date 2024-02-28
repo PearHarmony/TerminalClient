795bcecf13fc04796e84dca6aca80595605e105d
Chris <hi@aocalux.dev>
2024-02-28 19:48:43 +0100

    REMOVE OLD PROJEKT

--- src/org/pearharmony/App.java
+++
@@ -1,9 +0,0 @@
-package org.pearharmony;
-
-import org.pearharmony.control.Control;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        new Control();
-    }
-}
