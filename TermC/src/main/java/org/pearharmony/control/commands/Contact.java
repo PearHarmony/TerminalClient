b42c9d971b8e052d0db080c14c5cc172d3d5afa0
Chris <hi@aocalux.dev>
2024-03-05 10:57:23 +0100

    read write file

--- TermC/src/main/java/org/pearharmony/control/commands/Contact.java
+++ TermC/src/main/java/org/pearharmony/control/commands/Contact.java
@@ -4,27 +4,25 @@ package org.pearharmony.control.commands;
 
 import org.pearharmony.ui.TerminalMain;
 import java.io.*;
+import java.util.Scanner;
 
 public class Contact extends Command {
     private final String commandSyntax = "/contact <add:remove:edit> <name> (<ip>)";
 
-    private FileWriter contactsWrite;
-    private FileReader contactsRead;
+    private File contacts;
+    private Scanner contactsRead;
 
     public Contact(TerminalMain ui, String[] subCommands) {
         super(ui, "contact", subCommands);
         setSyntax(commandSyntax); // set general command syntax for syntax error
 
         try {
-            contactsWrite = new FileWriter("contacts");
+            contacts = new File("contacts");
+            contactsRead = new Scanner(contacts);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        try {
-            contactsRead = new FileReader("contacts");
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+
         decodeCommand();
     }
 
@@ -42,10 +40,19 @@ public class Contact extends Command {
 
     private void subEdit() { // edit contact list entry
         System.out.println("edit");
+        System.out.println(contactsRead.nextLine());
     }
 
     private void subAdd() { // add contact list entry
         System.out.println("add");
+
+        try {
+            FileWriter contactsWrite = new FileWriter(contacts);
+            contactsWrite.write("sdasdasdasd");
+            contactsWrite.close();
+        } catch (IOException e) {
+
+        }
     }
 
     private void subRemove() { // remove contact list entry
