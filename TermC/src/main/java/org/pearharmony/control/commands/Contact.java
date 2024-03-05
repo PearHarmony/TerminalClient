58d7270296d8a633b3143274cf8772c6271fd177
Chris <hi@aocalux.dev>
2024-03-05 22:15:48 +0100

    Update Contact.java

--- TermC/src/main/java/org/pearharmony/control/commands/Contact.java
+++ TermC/src/main/java/org/pearharmony/control/commands/Contact.java
@@ -2,8 +2,8 @@
 
 package org.pearharmony.control.commands;
 
-import org.pearharmony.ui.TerminalMain;
 import org.pearharmony.data.Contacts;
+import org.pearharmony.ui.TerminalMain;
 
 public class Contact extends Command {
     private Contacts contactBook;
@@ -22,7 +22,9 @@ public class Contact extends Command {
                 case "edit", "e" -> subEdit();
                 default -> errorSyntax();
             }
-        } else { errorSyntax(); }
+        } else {
+            errorSyntax();
+        }
     }
 
     private void subEdit() { // edit contact list entry
@@ -30,29 +32,42 @@ public class Contact extends Command {
             if (subCommands[2].equals(subCommands[3])) {
                 ui.displayError(4);
             } else if (contactBook.removeContact(subCommands[2]) == 0) {
-                if (contactBook.addContact(subCommands[2],subCommands[3]) == 0) {
+                if (contactBook.addContact(subCommands[2], subCommands[3]) == 0) {
                     ui.displayText(" Contact " + subCommands[2] + " has been changed");
+                } else {
+                    ui.displayError(6);
                 }
-                else { ui.displayError(6); }
-            } else { ui.displayError(6); }
-        } else { errorSyntax(); }
+            } else {
+                ui.displayError(6);
+            }
+        } else {
+            errorSyntax();
+        }
     }
 
     private void subAdd() { // add contact list entry
         if (subCommands.length == 4) {
             if (subCommands[2].equals(subCommands[3])) {
                 ui.displayError(4);
-            } else if (contactBook.addContact(subCommands[2],subCommands[3]) == 0) {
+            } else if (contactBook.addContact(subCommands[2], subCommands[3]) == 0) {
                 ui.displayText(" Contact added - Name: " + subCommands[2] + " IP: " + subCommands[3]);
-            } else { ui.displayError(3); }
-        } else { errorSyntax(); }
+            } else {
+                ui.displayError(3);
+            }
+        } else {
+            errorSyntax();
+        }
     }
 
     private void subRemove() { // remove contact list entry
         if (subCommands.length == 3) {
             if (contactBook.removeContact(subCommands[2]) == 0) {
                 ui.displayText(" The contact " + subCommands[2] + " has been deleted successfully.");
-            } else { ui.displayError(5); }
-        } else { errorSyntax(); }
+            } else {
+                ui.displayError(5);
+            }
+        } else {
+            errorSyntax();
+        }
     }
 }
