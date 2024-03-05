7fc3dd15700ac2597341a9c91c221b0a0a7dbdf0
Chris <hi@aocalux.dev>
2024-03-05 22:27:34 +0100

    Update AutoScrollTextBox.java

--- TermC/src/main/java/org/pearharmony/ui/lanternaFix/AutoScrollTextBox.java
+++ TermC/src/main/java/org/pearharmony/ui/lanternaFix/AutoScrollTextBox.java
@@ -4,7 +4,7 @@
     This is a custom implementation of the Lenterna TextBox component. An add line method has been added, witch adds the
     new line to the top inset of the bottom. If the text box is completely filed this can cas a lot of lag for that reasons
     the line count has been artificially limited within the addLine function.
- */
+*/
 package org.pearharmony.ui.lanternaFix;
 
 import com.googlecode.lanterna.TerminalSize;
