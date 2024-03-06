58ea12cd7e7654b22135ed1d73b2f683f6547307
Chris <hi@aocalux.dev>
2024-03-06 07:53:24 +0100

    Update MsgTrans.java

--- TermC/src/main/java/org/pearharmony/data/MsgTrans.java
+++ TermC/src/main/java/org/pearharmony/data/MsgTrans.java
@@ -7,8 +7,16 @@ public class MsgTrans {
     // startup init massage and var for Latest incoming massage
     private static String msg = "#: initialized successfully! \n" + "#: now running!";
 
-    public void setMsg(String _msg) {
-        msg = _msg;
+    public int setMsg(String _msg) {
+        // 0 -> sucsess
+        // 1 -> this function is not available, another massage is already queued
+
+        if (msg.isEmpty()) {
+            msg = _msg;
+            return 0;
+        } else {
+            return 1;
+        }
     } // set new incoming msg
 
     public String getMsg() {
